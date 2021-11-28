7ab0345142648a69886317be4a825d2a860436bc
ZICO <ziconinho.x@gmail.com>
2021-11-28 14:19:55 -0600

    applied reduce

--- util.js
+++ util.js
@@ -1,22 +1,12 @@
 const arr = [10,20,30,9,21,23,8,7,5,15,2,8,6,31];
-function sumOfElementGreaterThan20(){
-    let sum = 0;
-    for(let i=0;i<arr.length;i++){
-        if(arr[i] > 20){
-            sum += arr[i];
-        }
+function sumOfElementGreaterThan20(total, num){
+    if(num > 20){
+        total = total + num;
     }
-    return sum;
+    return total;
 }
-let sum = sumOfElementGreaterThan20();
-console.log(sum);
+console.log(arr.reduce(sumOfElementGreaterThan20,0));
 const getNewArr = function(arr){
-    let arrNew = [];
-    for(let i = 0;i <arr.length;i++){
-        if(arr[i].length > 5 && arr[i].indexOf('a') != -1){
-            arrNew.push(arr[i]);
-        }
-    }
-    return arrNew;
+   return arr.filter(s => s.length > 5 && s.indexOf('a') != -1);
 }
 console.log(getNewArr(['Rahmat','Yugal','Elias','Jaikishan']));
